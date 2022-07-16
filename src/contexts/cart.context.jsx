bdb1712501169b1837db1f4054fc897e292907be
Jakub Nowak <kubanowak9755@gmail.com>
2022-07-16 18:55:22 +0200

    feat: Implemented cartItems handling in the cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,30 @@
 import React, { createContext, useState } from 'react';
 
+const increaseCartItemQuantity = (cartItems, product) => {
+  const cartItemIndex = cartItems.findIndex(item => item.product.id === product.id);
+  if(cartItemIndex !== -1){
+    return cartItems.map((item, i) => i === cartItemIndex ? {...item, quantity: item.quantity + 1} : item);
+  } else {
+    return [...cartItems, {product: product, quantity: 1}];
+  }
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addCartItem: () => {},
 });
 
 export const CartContextProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const addCartItem = (product) => {
+    const updatedCart = increaseCartItemQuantity(cartItems, product);
+    setCartItems(updatedCart);
+  };
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addCartItem };
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>

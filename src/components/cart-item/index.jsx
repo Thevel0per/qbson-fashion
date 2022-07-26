e9b9c6e281f7f0e201897d2ab0c5802475bdfbec
Jakub Nowak <kubanowak9755@gmail.com>
2022-07-26 21:19:53 +0200

    feat: Create proper cart-item component

--- src/components/cart-item/index.jsx
+++ src/components/cart-item/index.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 const CartItem = ({cartItem}) => {
   const {product, quantity} = cartItem;
   return (
-    <div>
-      <h2>{ product.name }</h2>
-      <span>{ quantity }</span>
+    <div className='cart-item'>
+      <img className='cart-item__image' src={product.imageUrl} alt={product.name}/>
+      <div className='cart-item__details'>
+        <h2 className='cart-item__name'>{ product.name }</h2>
+        <span className='cart-item__price'>{ quantity } x ${product.price}</span>
+      </div>
     </div>
   );
 };

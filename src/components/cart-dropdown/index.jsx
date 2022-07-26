ef0eb732bfc6191593a45d4486c7e84acdb3b042
Jakub Nowak <kubanowak9755@gmail.com>
2022-07-26 23:42:54 +0200

    feat: Replaced Link with useNavigate to handle CHECKOUT button

--- src/components/cart-dropdown/index.jsx
+++ src/components/cart-dropdown/index.jsx
@@ -1,18 +1,23 @@
 import './cart-dropdown.styles.scss';
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppButton from '../app-button';
 import CartItem from '../cart-item';
 import { CartContext } from '../../contexts/cart.context';
-import { Link } from 'react-router-dom';
 
 const CartDropdown = () => {
+  const navigate = useNavigate();
+  const handleNavigateToCheckout = () => {
+    setIsCartOpen(false);
+    navigate('/checkout');
+  };
   const { cartItems, setIsCartOpen } = useContext(CartContext);
   return (
     <div className='cart-dropdown'>
       <ul className='cart-dropdown__items'>
         { cartItems.map((item, i) => <CartItem key={`cart-item-${i}`} cartItem={item} />) }
       </ul>
-      <Link to='/checkout' onClick={() => setIsCartOpen(false)}><AppButton className='cart-dropdown__button'>CHECKOUT</AppButton></Link>
+      <AppButton onClickHandler={handleNavigateToCheckout} className='cart-dropdown__button'>CHECKOUT</AppButton>
     </div>
   );
 };

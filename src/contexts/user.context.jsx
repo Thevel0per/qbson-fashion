50fa41605830e00ebf39dec68b9a2ac4b846e39c
Jakub Nowak <kubanowak9755@gmail.com>
2022-08-12 19:48:03 +0200

    feat: Update userContext to use reducer instead of useState

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener } from '../api/firebase/authetication';
 import { createUserDocumentFromAuth } from '../api/firebase/firestore';
 
@@ -8,10 +8,36 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  'SET_CURRENT_USER': 'SET_CURRENT_USER'
+};
+
+export const userReducer = (state, action) => {
+  console.log(`Dispatched ${action.type}!`);
+  const { type, payload } = action;
+
+  switch(type) {
+  case USER_ACTION_TYPES.SET_CURRENT_USER:
+    return { ...state, currentUser: payload };
+  default:
+    throw new Error(`Unknown action type: ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // Provider is a component that will wrap around any components that need the UserContext
 // It allows any children components to access the values inside of its state
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  console.log(currentUser);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {

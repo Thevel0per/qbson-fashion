7f2a4d1dea31f38631fc5abede929dea4a98497a
Jakub Nowak <kubanowak9755@gmail.com>
2022-09-10 21:11:40 +0200

    feat: set up redux cart store selectors

---
+++ src/store/cart/cart.selector.js
@@ -0,0 +1,30 @@
+import { createSelector } from 'reselect';
+
+const selectCartReducer = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCartReducer],
+  cart => cart.cartItems
+);
+
+
+export const selectIsCartOpen = createSelector(
+  [selectCartReducer],
+  cart => cart.isCartOpen
+);
+
+export const selectCartCount = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  )
+);
+
+export const selectCartTotalValue  = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (total, item) => total + item.quantity * item.product.price,
+    0
+  )
+);
